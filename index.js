c6ca3bcfffa9e761b722e3ca250db6d9ae206ecc
samagragupta <samagraguptajpr@gmail.com>
2019-02-13 08:02:55 +0530

    Added event listener to eavh id

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function click1(e) {
       {code:"document.getElementById('columns').style.backgroundColor='" + e.target.id + "'"});
       console.log(e.target.id);
 }
+function click2(e) {
+  chrome.tabs.executeScript(null,
+      // {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
+      {code:"document.getElementById('masthead').style.backgroundColor='" + e.target.id + "'"});
+      console.log(e.target.id);
+}
   
 document.addEventListener('DOMContentLoaded', function () {
   var td = document.querySelectorAll('td');
@@ -18,7 +24,10 @@ document.addEventListener('DOMContentLoaded', function () {
     td[i].addEventListener('click', click);
   }
   for (var i = 2; i < 4; i++) {
-    td[i].addEventListener('click1', click);
+    td[i].addEventListener('click', click1);
+  }
+  for (var i = 4; i < 6; i++) {
+    td[i].addEventListener('click', click2);
   }
 });
   
