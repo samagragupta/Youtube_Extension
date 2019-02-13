dffdf495b516809c2c1302fd30b7e0ddc402b05d
samagragupta <samagraguptajpr@gmail.com>
2019-02-13 07:59:34 +0530

    Added event listener

---
+++ index.js
@@ -0,0 +1,24 @@
+function click(e) {
+  chrome.tabs.executeScript(null,
+      // {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
+      {code:"document.getElementById('secondary').style.backgroundColor='" + e.target.id + "'"});
+      console.log(e.target.id);
+}
+
+function click1(e) {
+  chrome.tabs.executeScript(null,
+      // {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
+      {code:"document.getElementById('columns').style.backgroundColor='" + e.target.id + "'"});
+      console.log(e.target.id);
+}
+  
+document.addEventListener('DOMContentLoaded', function () {
+  var td = document.querySelectorAll('td');
+  for (var i = 0; i < 2; i++) {
+    td[i].addEventListener('click', click);
+  }
+  for (var i = 2; i < 4; i++) {
+    td[i].addEventListener('click1', click);
+  }
+});
+  
